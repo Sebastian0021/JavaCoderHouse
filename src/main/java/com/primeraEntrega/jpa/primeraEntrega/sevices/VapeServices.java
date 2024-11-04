be6f9131208456fea5d498711563d2ea260f7980
Sebastian0021 <sebastianalefuentespe@gmail.com>
2024-11-04 19:57:36 -0300

    Andando...

--- src/main/java/com/primeraEntrega/jpa/primeraEntrega/sevices/VapeServices.java
+++ src/main/java/com/primeraEntrega/jpa/primeraEntrega/sevices/VapeServices.java
@@ -22,11 +22,31 @@ public class VapeServices {
 	}
 
 	public Vape getVapeById(long id) {
-		return VapeRepository.findById(id).orElse(null);
+		return VapeRepository.findById(id).orElseThrow(() -> new RuntimeException("Vape not found"));
 	}
 
 	public List<Vape> getVapes() {
 		return VapeRepository.findAll();
+	}
+
+	public void updateVapeById(long id, Vape vape){
+		Vape v = this.getVapeById(id);
+
+		v.setName(vape.getName());
+		v.setDescription(vape.getDescription());
+		v.setPrice(vape.getPrice());
+		v.setStock(vape.getStock());
+		v.setBrand(vape.getBrand());
+		v.setFlavor(vape.getFlavor());
+		v.setPuffs(vape.getPuffs());
+
+		VapeRepository.save(v);
+	}
+
+	public Vape deleteVapeById(long id) {
+		Vape v = this.getVapeById(id);
+		VapeRepository.delete(v);
+		return v;
 	}
 	
 }
